fe2e3c291b810715e7f4fa0c5e2db42b1a9d66f8
Soham <sohamkanoj55@gmail.com>
2025-12-10 21:18:50 +0530

    reverted to backend.hcl

--- iac/environments/prod/resourcegroup/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "azurerm" {
-    resource_group_name  = "azure-saas-project-rg"
-    storage_account_name = "azuresaasproject"
-    container_name       = "prod"
-    key                  = "resource_group.tfstate"
-  }
-}
