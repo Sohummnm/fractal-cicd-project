45fb54ca3c0585eda1e84438883b6ad33f569f59
Soham <sohamkanoj55@gmail.com>
2025-12-10 20:31:26 +0530

    fixes

--- iac/environments/prod/resourcegroup/outputs.tf
+++ iac/environments/prod/resourcegroup/outputs.tf
@@ -1,16 +1,16 @@
 output "rg_name" {
   description = "name of the resource group"
-  value = module.rg.rg_name
+  value = azurerm_resource_group.rg.name
   
 }
 
 output "rg_location" {
   description = "location of the resource group"
-  value =module.rg.rg_location
+  value = azurerm_resource_group.rg.location
 }
 
 output "rg_id" {
   description = "id of the resource group"
-  value = module.rg.rg_id
+  value = azurerm_resource_group.rg.id
 }
 
